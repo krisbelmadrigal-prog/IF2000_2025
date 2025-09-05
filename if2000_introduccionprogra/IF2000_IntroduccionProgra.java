4802455da5e306735b8ff08c2a3dbe559dd7cc5c
krisbelmadrigal-prog <krisbelmadrigal@gmail.com>
2025-09-04 21:50:41 -0600

    Add files via upload
    "Main"

---
+++ if2000_introduccionprogra/IF2000_IntroduccionProgra.java
@@ -0,0 +1,192 @@
+package if2000_introduccionprogra;
+
+import logic.Ejercicios_Semana2_PracticaEnClase;
+import logic.Laboratorio_1;
+import logic.Quiz_3;
+import logic.Laboratorio_2;
+
+
+/**
+ *
+ * @author krisb
+ */
+public class IF2000_IntroduccionProgra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        /*
+    String name= "Joseph";
+    int age=40;
+    /*tipos de datos numericos
+    *int,long,short,float,double,numeric,money,decimal
+    *
+    *
+manejo de fechas
+date
+cadenas de caracteres:
+String
+
+objetos
+object
+
+data structures 
+array or vectors: int[] x;
+                  int[] y;
+
+true/false
+boolean
+*/
+        /*
+        System.out.println("HOLA MUNDO");
+        System.out.println("Nombre:" +name);
+        System.out.println("Edad:"+age);
+        
+    }*/
+    
+    
+    Ejercicios_Semana2_PracticaEnClase ej;
+    Ejercicios_Semana2_PracticaEnClase ej2;
+    Ejercicios_Semana2_PracticaEnClase ej3;
+    Ejercicios_Semana2_PracticaEnClase ej4;
+    Ejercicios_Semana2_PracticaEnClase ej5;
+    Ejercicios_Semana2_PracticaEnClase ej6;
+    Ejercicios_Semana2_PracticaEnClase ej7;
+    Quiz_3 ej8;
+    Laboratorio_1 ej9;
+    Laboratorio_1 ej10;
+    Laboratorio_1 ej11;
+    Laboratorio_1 ej12;
+    Laboratorio_1 ej13;
+    Laboratorio_1 ej14;
+    Laboratorio_1 ej15;
+    Laboratorio_1 ej16;
+    Laboratorio_1 ej17;
+    Laboratorio_1 ej18;
+    Laboratorio_1 ej19;
+    Laboratorio_1 ej20;
+    Laboratorio_2 ej21;
+    Laboratorio_2 ej22;
+    Laboratorio_2 ej23;
+    Laboratorio_2 ej24;
+    Laboratorio_2 ej25;
+    Laboratorio_2 ej26;
+    Laboratorio_2 ej27;
+    Laboratorio_2 ej28;
+    Laboratorio_2 ej29;
+    Laboratorio_2 ej30;
+    Laboratorio_2 ej31;
+    Laboratorio_2 ej32;
+    Laboratorio_2 ej33;
+    Laboratorio_2 ej34;
+    Laboratorio_2 ej35;
+    
+    ej = new Ejercicios_Semana2_PracticaEnClase();
+    ej.quiz2_B_piramide(5);
+    
+    ej2= new Ejercicios_Semana2_PracticaEnClase();
+    ej2.ejercicio_A();
+    
+    ej3= new Ejercicios_Semana2_PracticaEnClase();
+    ej3.ejercicio_B(12);
+    
+    ej4= new Ejercicios_Semana2_PracticaEnClase();
+    ej4.ejercicio_C(10);
+    
+    ej5= new Ejercicios_Semana2_PracticaEnClase();
+    ej5.ejercicio_D();
+    
+    ej6 = new Ejercicios_Semana2_PracticaEnClase();
+    ej6.ejercicio_E();
+    
+    ej7 = new Ejercicios_Semana2_PracticaEnClase();
+    ej7.ejercicio_F();
+    
+    ej8 = new Quiz_3();
+    ej8.quiz3();
+    
+    ej9= new Laboratorio_1();
+    ej9.ejercicio_1();
+    
+    ej10= new Laboratorio_1();
+    ej10.ejercicio_2();
+    
+    ej11= new Laboratorio_1();
+    ej11.ejercicio_3();
+    
+    ej12= new Laboratorio_1();
+    ej12.ejercicio_4();
+    
+    ej13= new Laboratorio_1();
+    ej13.ejercicio_5();
+    
+    ej14= new Laboratorio_1();
+    ej14.ejercicio_6();
+    
+    ej15= new Laboratorio_1();
+    ej15.ejercicio_7();
+    
+    ej16= new Laboratorio_1();
+    ej16.ejercicio_8();
+    
+    ej17= new Laboratorio_1();
+    ej17.ejercicio_9();
+    
+    ej18= new Laboratorio_1();
+    ej18.ejercicio_10();
+    
+    ej19= new Laboratorio_1();
+    ej19.ejercicio_11();
+    
+    ej20= new Laboratorio_1();
+    ej20.ejercicio_12();
+    
+    ej21= new Laboratorio_2();
+    ej21.ejercicio_13();
+    
+    ej22= new Laboratorio_2();
+    ej22.ejercicio_14();
+    
+    ej23= new Laboratorio_2();
+    ej23.ejercicio_15();
+    
+    ej24= new Laboratorio_2();
+    ej24.ejercicio_16();
+    
+    ej25= new Laboratorio_2();
+    ej25.ejercicio_17();
+    
+    ej26= new Laboratorio_2();
+    ej26.ejercicio_18();
+    
+    ej27= new Laboratorio_2();
+    ej27.ejercicio_19();
+    
+    ej28= new Laboratorio_2();
+    ej28.ejercicio_20();
+    
+    ej29= new Laboratorio_2();
+    ej29.ejercicio_21();
+    
+    ej30= new Laboratorio_2();
+    ej30.ejercicio_22();
+    
+    ej31= new Laboratorio_2();
+    ej31.ejercicio_23();
+    
+    ej32= new Laboratorio_2();
+    ej32.ejercicio_24();
+    
+    ej33= new Laboratorio_2();
+    ej33.ejercicio_25();
+    
+    ej34= new Laboratorio_2();
+    ej34.ejercicio_26();
+    
+    ej35= new Laboratorio_2();
+    ej35.ejercicio_27();
+}
+}
+
